954576f6fabf0e638c8f9c50655b3c84130f6b47
fws1998 <404749592@qq.com>
2022-01-02 22:04:24 +1100

    init

--- read_file.py
+++ read_file.py
@@ -1,6 +1,14 @@
 import json
+import emoji
 
 with open("test.json", 'r', encoding='UTF-8') as file:
     room_info = json.load(file)
+for i in room_info:
+    time_stamp = room_info[i]["time"]
+    body = json.loads(room_info[i]["custom"])
+    type = room_info[i]["type"]
 
-print(room_info[0]["custom"])
+
+
+def photo(timestamp, body):
+    id = timestamp
